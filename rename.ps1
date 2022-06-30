a24f452d25555b32c8b98d1802511f6bdf835f8f
Aleksey Baryshkov <barishkov@gmail.com>
2022-06-30 11:04:57 +0000

    Added #

--- rename.ps1
+++ rename.ps1
@@ -1,2 +1,12 @@
+
+# --------------------------------------------
+# Script by Aleksey B.
+#
+#
+# Version        Date           Info
+# 1.0         30.06.2022    Initial Version
+#
+# --------------------------------------------
+
 $csv_data = Import-Csv -Path C:\ps\rename_users.csv -Delimiter ";"
 $csv_data | % {Set-ADUser -Identity $_.SamAccountName -City $_.City -Company $_.Company -Country $_.Country -Department $_.Department -Description $_.Description -displayName $_.displayName -givenName $_.givenName -Initials $_.Initials -SurName $_.SurName -Title $_.Title}
